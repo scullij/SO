f02dfe30caf7e749d6ce3a368c1d6e5136c0e451
Javier Sculli <javier.sculli@gmail.com>
2013-04-24 01:29:56 -0300

    Project init

---
+++ Debug/src/main.d
@@ -0,0 +1,4 @@
+src/main.d: ../src/main.c \
+ /home/utnso/workspace/so-commons-library/commons/temporal.h
+
+/home/utnso/workspace/so-commons-library/commons/temporal.h:
---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : OSGroup
+ Version     :
+ Copyright   : Your copyright notice
+ Description : TP
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <commons/temporal.h> // Shared library configurada
+
+int main(void) {
+	puts("Hola Operativos!");
+	return EXIT_SUCCESS;
+}
